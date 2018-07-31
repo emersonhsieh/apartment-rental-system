6efcc36d66e7d8029f73b6f737216a9bb0f24557
Hantao (WIll) Wang <hantaowang97@gmail.com>
2018-07-30 21:11:05 -0700

    bug fixes pt 2

--- quilt-spec/main.js
+++ quilt-spec/main.js
@@ -2,14 +2,16 @@ const quilt = require('@quilt/quilt');
 const nodeServer = require('./nodeServer3PM');
 const machineFactory = require('./machines')
 
-const deployment = quilt.createDeployment({namespace: "TEMP", adminACL: ['0.0.0.0/0']});
+const githubKey = "hantaowang"
 
-var machines = new machineFactory(7, "hantaowang")
+const deployment = quilt.createDeployment({namespace: githubKey + "-apartment-app", adminACL: ['0.0.0.0/0']});
+
+var machines = new machineFactory(7, githubKey)
 
 var countNode = 3;
 const nodeRepository = 'tsaianson/node-apt-app';
 const apartmentApp = new nodeServer(countNode, nodeRepository);
 
-apartmentApp.machPlacements(machines.getSizes());
+apartmentApp.matchPlacements(machines.getSizes());
 
 deployment.deploy(apartmentApp);
